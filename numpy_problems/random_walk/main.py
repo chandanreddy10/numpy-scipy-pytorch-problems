9f08741d2f6c6e427c420a03775317d379bfe545
chandanreddy10 <chandanreddy10@outlook.com>
2026-03-27 22:31:37 +0100

    added gaussian random walk

--- numpy_problems/random_walk/main.py
+++ numpy_problems/random_walk/main.py
@@ -4,8 +4,10 @@ import os
 from pathlib import Path 
 
 START=0
-END_AT=10
+END_AT=100
 DATA_DIR="misc"
+MEAN=0
+VARIANCE=1
 
 def elementary_random_walk(start=START,end=END_AT):
     path = []
@@ -19,16 +21,29 @@ def elementary_random_walk(start=START,end=END_AT):
     
     return path
 
-def plot_random_walk(path):
+def gauss_random_walk(start=START,end=END_AT, mean=MEAN, variance=VARIANCE):
+    path = []
+    old_step=START
+    index= 0 
+    while old_step < end:
+        print(f"Step Count : {index} | Value : {old_step}")
+        path.append(old_step)
+        next_step = np.random.normal(MEAN, VARIANCE)
+        old_step = old_step + next_step
+    
+    return path
+
+def plot_random_walk(path, filename="random_walk"):
     x=np.arange(0, len(path))
     
     plt.plot(x, path)
     plt.xlabel("Steps")
     plt.ylabel("Value")
     plt.title(f"Random walk steps : {len(path)}")
+    plt.savefig(f"misc/{filename}.jpg")
     plt.show()
-    plt.savefig("misc/random_walk.jpg", dpi=300)
-
     print("Saved Fig")
 path = elementary_random_walk()
 plot_random_walk(path)
+path = gauss_random_walk()
+plot_random_walk(path, filename="gauss_random_walk")
