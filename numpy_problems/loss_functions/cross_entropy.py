6d887caf34376fe40a4894ee9a38057539d0ad52
chandanreddy10 <chandanreddy10@outlook.com>
2026-03-30 21:42:32 +0200

    grad cross entropy

--- numpy_problems/loss_functions/cross_entropy.py
+++ numpy_problems/loss_functions/cross_entropy.py
@@ -6,11 +6,17 @@ def softmax(logits):
     exp_logits = np.exp(logits)
     return exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
 
-def cross_entropy(y_logits):
+def cross_entropy(y_logits, y_true):
     y_logits_softmax = softmax(y_logits)
     probs = y_logits_softmax[np.arange(len(y_true)), y_true]
     log_prob = -np.log(probs)
     return np.mean(log_prob)
+
+def grad_cross_entropy(y_logits, y_true):
+    y_logits_softmax = softmax(y_logits)
+    y_logits_softmax[np.arange(len(y_true)), y_true] = y_logits_softmax[np.arange(len(y_true)), y_true] - 1
+
+    return y_logits_softmax / len(y_true)
 
 y_logits=np.array([
     [0.0, 5.0, 0.0, 0.0],
@@ -19,4 +25,5 @@ y_logits=np.array([
     [0.0, 1.0, 1.0, 5.0]
 ])
 y_true = np.array([0,1,2,3])
-print(cross_entropy(y_logits))
+print(cross_entropy(y_logits, y_true))
+print(grad_cross_entropy(y_logits, y_true))
