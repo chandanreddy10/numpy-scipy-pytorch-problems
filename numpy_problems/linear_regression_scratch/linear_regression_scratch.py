c67d660e14024515f214556ac88a5d609d9e21e0
chandanreddy10 <chandanreddy10@outlook.com>
2026-03-30 22:36:19 +0200

    lin reg

--- numpy_problems/linear_regression_scratch/linear_regression_scratch.py
+++ numpy_problems/linear_regression_scratch/linear_regression_scratch.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd 
 
 TEST_SIZE=0.2
+LEARNING_RATE=0.01
+BATCH_SIZE=1
+EPOCHS=1
 #Boston Housing Dataset load
 df = pd.read_csv("BostonHousing.csv")
 total_records = df.shape[0]
@@ -10,7 +13,15 @@ test_records_size = int(TEST_SIZE * total_records)
 test_record_indices = np.random.choice(np.arange(total_records), test_records_size)
 test_data = df.iloc[test_record_indices]
 train_data = df.drop(index=test_record_indices)
+train_data = train_data.sample(frac=1)
+y_test = test_data.iloc[:,-1]
+X_test = test_data.drop(columns="medv")
+y_train = train_data.iloc[:, -1]
+X_train = train_data.drop(columns="medv")
 
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+random_coef = np.random.sample(X_train.shape[1])
+print(random_coef)
 def optimize():
     pass
 
@@ -20,4 +31,20 @@ def gradient():
 def loss_function():
     pass
 
-    
+def data_batch(X_train, y_train, BATCH_SIZE=BATCH_SIZE):
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    dataset = []
+    for index in range(0,X_train.shape[0],BATCH_SIZE):
+        X_temp = X_train[index:index+BATCH_SIZE].flatten()
+        y_temp = y_train[index:index+BATCH_SIZE]
+        dataset.append((X_temp, y_temp))
+    return dataset
+
+train_dataset = data_batch(X_train, y_train)
+print(train_dataset)
+for epoch in range(EPOCHS):
+    for x, y in train_dataset:
+        print(x.shape,y)
+        break
+
