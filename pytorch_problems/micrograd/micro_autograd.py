a843089332da41a2aac6b29ec448acfbad42130a
chandanreddy10 <chandanreddy10@outlook.com>
2026-04-01 21:53:51 +0200

    basic graph

--- pytorch_problems/micrograd/micro_autograd.py
+++ pytorch_problems/micrograd/micro_autograd.py
@@ -5,7 +5,7 @@ import numpy as np
 class GradObject:
     def __init__(self, value):
         self.value = value
-    
+        self.grad = 0
     def __add__(self, other):
         return GradObject(self.value + other.value)
     
@@ -23,10 +23,10 @@ a = x1 * x2
 y = x1 + x2 + a 
 
 graph = nx.DiGraph()
-graph.add_node("x1",layer=0, value=x1.value)
-graph.add_node("x2",layer=0,  value=x2.value)
-graph.add_node("a", layer=1, value=a.value)
-graph.add_node("y",layer=2,  value=y.value)
+graph.add_node("x1",layer=0, value=f"Value:{x1.value}\nGrad:{x1.grad}")
+graph.add_node("x2",layer=0,  value=f"Value:{x2.value}\nGrad:{x2.grad}")
+graph.add_node("a", layer=1, value=f"Op:+\nValue:{a.value}\nGrad:{a.grad}")
+graph.add_node("y",layer=2,  value=f"Op:+\nValue:{y.value}\nGrad:{y.grad}")
 
 graph.add_edge("x1", "a")
 graph.add_edge("x2", "a")
@@ -36,5 +36,5 @@ graph.add_edge("x2", "y")
 
 pos = nx.multipartite_layout(graph, subset_key="layer")
 labels = {n: f"{n}\n{graph.nodes[n]['value']}" for n in graph.nodes()}
-nx.draw(graph, pos, with_labels=True, labels=labels, node_size=1500, node_color='lightblue', font_size=10)
+nx.draw(graph, pos, with_labels=True, labels=labels, node_size=5000, node_color='lightblue', font_size=10)
 plt.show()
