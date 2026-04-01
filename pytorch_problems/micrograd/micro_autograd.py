2bf063189dd3dd65dc548108ace0519c97651338
chandanreddy10 <chandanreddy10@outlook.com>
2026-04-01 21:22:02 +0200

    init grad object

---
+++ pytorch_problems/micrograd/micro_autograd.py
@@ -0,0 +1,22 @@
+import numpy as np 
+import networkx as glib
+
+class GradObject:
+    def __init__(self, value):
+        self.value = value
+    
+    def __add__(self, other):
+        print("Operator overloading for gradient calculation")
+        return self.value + other.value
+    def __mul__(self, other):
+        return self.value * other.value
+    def __repr__(self):
+        pass
+
+#Example 
+x1 = GradObject(2)
+x2 = GradObject(3)
+
+print(x1+x2, x1*x2, (x1*x2 + x2*x1))
+# y = x1*x2 + x1**2 + np.log(x2)
+
