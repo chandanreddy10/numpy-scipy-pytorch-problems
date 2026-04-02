24111cd5b4cf1739aab545fc09462e4449fa34e0
chandanreddy10 <chandanreddy10@outlook.com>
2026-04-02 20:01:18 +0200

    basic autograd added

--- pytorch_problems/micrograd/main.py
+++ pytorch_problems/micrograd/main.py
@@ -4,7 +4,31 @@ class GradObject:
     def __init__(self,value):
         self.value = value 
         self.grad = 0
-    
+        self.parents = []
+        self._backward = lambda : None
     def __add__(self, other):
-        pass 
+        out = GradObject(self.value + other.value)
+        out.parents = [self, other]
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad 
+        out._backward = _backward
+
+        return out 
+    
+    def backward(self, grad=None):
+        if grad is None:
+            grad = np.ones_like(self.value)
+        self.grad += grad
+        self._backward()
+
+a = GradObject(np.array(2.0))
+b = GradObject(np.array(3.0))
+
+c = a + b
+c.backward()
+
+print(a.grad)
+print(b.grad) 
 
