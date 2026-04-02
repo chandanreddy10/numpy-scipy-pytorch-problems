a5a127e093a076072caa4c7b1f79795dff632e1f
chandanreddy10 <chandanreddy10@outlook.com>
2026-04-02 20:39:07 +0200

    topo order added

--- pytorch_problems/micrograd/main.py
+++ pytorch_problems/micrograd/main.py
@@ -1,11 +1,12 @@
 import numpy as np 
 
 class GradObject:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value 
         self.grad = 0
         self.parents = []
         self._backward = lambda : None
+
     def __add__(self, other):
         out = GradObject(self.value + other.value)
         out.parents = [self, other]
@@ -17,18 +18,37 @@ class GradObject:
 
         return out 
     
+    def __mul__(self, other):
+        out = GradObject(self.value * other.value)
+        out.parents = [self, other]
+
+        def _backward():
+            self.grad += other.value * out.grad
+            other.grad += self.value * out.grad
+        
+        out._backward=_backward
+        return out
+
     def backward(self, grad=None):
-        if grad is None:
-            grad = np.ones_like(self.value)
-        self.grad += grad
-        self._backward()
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v.parents:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
 
 a = GradObject(np.array(2.0))
 b = GradObject(np.array(3.0))
 
-c = a + b
+c = a * b + a * b
 c.backward()
-
 print(a.grad)
 print(b.grad) 
 
